77602575b8a8f6da02db75a4ea4b36457c4eacd9
kundankumar-gupta <kkg7366856141@gmail.com>
2022-10-13 13:22:01 +0530

    added switch case

---
+++ switch-case.js
@@ -0,0 +1,88 @@
+var day = 4;
+switch (day) {
+  case 0:
+    console.log("today is monday");
+    break;
+
+  case 1:
+    console.log("today is tuesday");
+    break;
+
+  case 2:
+    console.log("today is wednesday");
+    break;
+
+  case 3:
+    console.log("today is thursday");
+    break;
+
+  case 4:
+    console.log("today is friday");
+    break;
+
+  case 5:
+    console.log("today is saturady");
+    break;
+
+  case 6:
+    console.log("today is sunday");
+    break;
+
+  default:
+    console.log("enter the valid  week day");
+}
+
+var months = 9;
+
+switch (months) {
+  case 1:
+    console.log("january");
+    break;
+
+  case 2:
+    console.log("feb");
+    break;
+
+  case 3:
+    console.log("march");
+    break;
+
+  case 4:
+    console.log("april");
+    break;
+
+  case 5:
+    console.log("may");
+    break;
+
+  case 6:
+    console.log("june");
+    break;
+
+  case 7:
+    console.log("july");
+    break;
+
+  case 8:
+    console.log("august");
+    break;
+
+  case 9:
+    console.log("september");
+    break;
+
+  case 10:
+    console.log("oct");
+    break;
+
+  case 11:
+    console.log("november");
+    break;
+
+  case 12:
+    console.log("december");
+    break;
+
+  default:
+    console.log("enter valid monts name");
+}
