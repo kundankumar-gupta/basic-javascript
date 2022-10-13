63556830823e1b7b8bef19047b1b55c9645ec887
kundankumar-gupta <kkg7366856141@gmail.com>
2022-10-13 12:03:50 +0530

    added if statement

---
+++ if-statement.js
@@ -0,0 +1,12 @@
+const x = 15;
+if (x > 10) {
+  console.log("x is greater");
+}
+if (x < 20) {
+  console.log("x is smaller");
+}
+const a = 200;
+const b = 100;
+if (a > b) {
+  console.log("hello yahoobaba");
+}
