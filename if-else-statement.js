39d6a3e5d06c3aaeee4918b16aeaa911776acdc7
kundankumar-gupta <kkg7366856141@gmail.com>
2022-10-13 12:40:58 +0530

    added if-else-statement

--- if-else-statement.js
+++ if-else-statement.js
@@ -4,3 +4,26 @@ if (x > 18) {
 } else {
   console.log("you are a child");
 }
+const z = 15;
+if (z > 20) {
+  console.log("x is greater");
+} else {
+  console.log("x is smaller");
+}
+if (z > 10) {
+  console.log("x is greater");
+} else {
+  console.log("x is smaller");
+}
+if (z == 15) {
+  console.log("z is same");
+} else {
+  console.log("z is not same");
+}
+const name = "kundan";
+const gender = "male";
+if (gender == "male") {
+  console.log("hello mr." + name);
+} else {
+  console.log("hello miss" + name);
+}
